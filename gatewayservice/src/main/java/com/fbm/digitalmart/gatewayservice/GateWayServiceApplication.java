32cb6c388ebeb193a2d92edbf766790018efae9f
farshadmlp <farshadmlp@gmail.com>
2022-04-14 14:51:00 +0530

    gatewayservice implementation

--- gatewayservice/src/main/java/com/fbm/digitalmart/gatewayservice/GateWayServiceApplication.java
+++ gatewayservice/src/main/java/com/fbm/digitalmart/gatewayservice/GateWayServiceApplication.java
@@ -2,12 +2,14 @@ package com.fbm.digitalmart.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class GatewayserviceApplication {
+@EnableEurekaClient
+public class GateWayServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GatewayserviceApplication.class, args);
+		SpringApplication.run(GateWayServiceApplication.class, args);
 	}
 
 }
